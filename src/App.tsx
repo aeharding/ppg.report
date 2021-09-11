8899e771a5dac583e0dbd0a9c0202b36af63fdfa
Alexander Harding <2166114+aeharding@users.noreply.github.com>
2021-09-11 16:23:02 -0500

    Add initial update available work

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import HeaderRoutes from "./header/web/HeaderRoutes";
 import { css, Global } from "@emotion/react/macro";
 import { writeVariables } from "./theme";
 import { isInstalled } from "./helpers/device";
+import Updater from "./Updater";
 
 const Background = styled.div`
   position: fixed;
@@ -76,6 +77,8 @@ function App() {
           <Footer />
         </AppContents>
       </AppContainer>
+
+      <Updater />
     </Router>
   );
 }
