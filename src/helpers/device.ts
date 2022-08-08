edec7d5db95e83d34da024cd1981bdd081cec077
Alexander Harding <2166114+aeharding@users.noreply.github.com>
2022-08-08 17:36:15 -0500

    Add PWA cookie detection

--- src/helpers/device.ts
+++ src/helpers/device.ts
@@ -2,6 +2,17 @@ export function isTouchDevice(): boolean {
   return window.matchMedia("(any-hover: none)").matches;
 }
 
+function parseCookies(): Record<string, string | undefined> {
+  return Object.fromEntries(
+    document.cookie.split("; ").map((x) => x.split("="))
+  );
+}
+
+const pwaBuilderCookie = parseCookies()["app-platform"];
+
 export function isInstalled(): boolean {
-  return window.matchMedia("(display-mode: standalone)").matches;
+  return (
+    window.matchMedia("(display-mode: standalone)").matches ||
+    pwaBuilderCookie === "iOS App Store"
+  );
 }
