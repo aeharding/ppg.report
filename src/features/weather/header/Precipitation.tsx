ebf148a30fc81f79d6fdf226e38b642ec07a740e
Alexander Harding <2166114+aeharding@users.noreply.github.com>
2022-09-02 23:11:39 -0500

    add snow

--- src/features/weather/header/Precipitation.tsx
+++ src/features/weather/header/Precipitation.tsx
@@ -1,4 +1,4 @@
-import { css } from "@emotion/react/macro";
+import { css, keyframes } from "@emotion/react/macro";
 import styled from "@emotion/styled/macro";
 import { faRaindrops } from "@fortawesome/pro-duotone-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -9,6 +9,29 @@ import { outputP3ColorFromRGBA } from "../../../helpers/colors";
 import { findValue } from "../../../services/weather";
 import { HeaderType, Micro } from "../WeatherHeader";
 import { WeatherResult } from "../weatherSlice";
+
+const snow = keyframes` 
+  0% {
+    background-position: 0px 0px, 0px 0px, 0px 0px;
+  }
+  100% {
+    background-position: 500px 1000px, 400px 400px, 300px 300px;
+  }
+`;
+
+const Snow = styled.div<{ chance: number }>`
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+  background-image: url("https://cdn.discordapp.com/attachments/744182992037216260/990023034524033105/snow1.png"),
+    url("https://cdn.discordapp.com/attachments/744182992037216260/990023034758922250/snow2.png"),
+    url("https://cdn.discordapp.com/attachments/744182992037216260/990023034951827556/snow3.png");
+  z-index: -1;
+  opacity: ${({ chance }) => chance / 2};
+
+  animation: ${snow} 10s linear infinite;
+`;
 
 const colorScale = chroma
   .scale(["#ffffff88", "#ffffffff", "#006affff"])
@@ -60,20 +83,27 @@ export default function Precipitation({
   if (chance.value < 5) return <></>;
 
   return (
-    <Tippy content={`${chance.value}% chance precipitation`} placement="bottom">
-      <div>
-        <Micro
-          icon={
-            <RainIcon
-              headerType={headerType}
-              icon={faRaindrops}
-              chance={chance.value}
-            />
-          }
-        >
-          {body}
-        </Micro>
-      </div>
-    </Tippy>
+    <>
+      {chance.value > 50 && <Snow chance={chance.value / 100} />}
+
+      <Tippy
+        content={`${chance.value}% chance precipitation`}
+        placement="bottom"
+      >
+        <div>
+          <Micro
+            icon={
+              <RainIcon
+                headerType={headerType}
+                icon={faRaindrops}
+                chance={chance.value}
+              />
+            }
+          >
+            {body}
+          </Micro>
+        </div>
+      </Tippy>
+    </>
   );
 }
