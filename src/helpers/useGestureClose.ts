4ed89691eb11a0ee74ffbf360962540c78d99fe2
Alexander Harding <2166114+aeharding@users.noreply.github.com>
2023-06-07 19:37:06 -0500

    Dismiss tooltip on touch gesture

---
+++ src/helpers/useGestureClose.ts
@@ -0,0 +1,33 @@
+import { FloatingContext, ReferenceType } from "@floating-ui/react";
+import { useEffect } from "react";
+
+interface GestureCloseOptions {
+  interactive?: boolean;
+}
+
+export default function useGestureClose<
+  RT extends ReferenceType = ReferenceType
+>(
+  context: FloatingContext<RT>,
+  { interactive }: GestureCloseOptions = { interactive: false }
+) {
+  const { open, onOpenChange } = context;
+
+  useEffect(() => {
+    if (!open) return;
+
+    const handleTouchStart = () => {
+      onOpenChange(false);
+    };
+
+    document.body.addEventListener("touchmove", handleTouchStart);
+    if (!interactive)
+      document.body.addEventListener("touchstart", handleTouchStart);
+
+    return () => {
+      document.body.removeEventListener("touchmove", handleTouchStart);
+      if (!interactive)
+        document.body.removeEventListener("touchstart", handleTouchStart);
+    };
+  }, [open, onOpenChange, interactive]);
+}
